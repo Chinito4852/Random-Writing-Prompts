8836f186062021307fcb91794cb25efe52c403fd
Chinito4852 <tristinowong@gmail.com>
2021-02-10 17:37:48 -0800

    added database configs

--- backend/settings.py
+++ backend/settings.py
@@ -14,9 +14,16 @@ from pathlib import Path
 from .secret import k
 from corsheaders.defaults import default_headers
 import os
+import django_heroku
+import dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
@@ -82,13 +89,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+'''
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -154,3 +164,8 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 ]
 
 FILE_CHARSET = 'utf-8'
+
+django_heroku.settings(locals())
+
+options = DATABASES['default'].get('OPTIONS', {})
+options.pop('sslmode', None)
